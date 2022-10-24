20b662e75f675ec79f7a0ddcda4b64d134492d1b
daridev <hernandezdarifrancisco@gmail.com>
2022-10-24 11:57:00 -0500

    pyramid start app

--- __main__.py
+++ __main__.py
@@ -1,10 +1,38 @@
 import os
-from config import Config
-from logs import logger
-from scraping_manager.automate import Web_scraping
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+from supabase import create_client as sb_create_client, Client as sb_client
+from config import Config as Credentials
 
-def main (): 
+
+# Get credentials from config
+credentials = Credentials()
+public = credentials.get ("supabase_public")
+secret = credentials.get ("supabase_secret")
+project_url = credentials.get ("supabase_project_url")
+
+# Conncect to Supabase
+supabase: sb_client = sb_create_client(project_url, secret)
+
+def hello_world(request):
+    print('Incoming request')
+    return Response('<body><h1>Hello World!</h1></body>')
 
 
-if __name__ == "__main__":
-    main()
+# Setup and start app
+if __name__ == '__main__':
+    with Configurator() as config:
+        
+        # app includes
+        config.include("pyramid_jinja2")
+        
+        # add rtoutes
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        
+        # Setup wsgi
+        app = config.make_wsgi_app()
+        
+    # Run
+    make_server(host='0.0.0.0', port=6543, app=app).serve_forever()
