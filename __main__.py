2ae51072c379798d0394c5b48377cd74501ab623
daridev <hernandezdarifrancisco@gmail.com>
2022-10-24 12:47:15 -0500

    render templates

--- __main__.py
+++ __main__.py
@@ -5,7 +5,6 @@ from pyramid.response import Response
 from supabase import create_client as sb_create_client, Client as sb_client
 from config import Config as Credentials
 
-
 # Get credentials from config
 credentials = Credentials()
 public = credentials.get ("supabase_public")
@@ -15,21 +14,35 @@ project_url = credentials.get ("supabase_project_url")
 # Conncect to Supabase
 supabase: sb_client = sb_create_client(project_url, secret)
 
-def hello_world(request):
-    print('Incoming request')
-    return Response('<body><h1>Hello World!</h1></body>')
+def home(request):
+    context = {}
+    return context
 
+def signup(request):
+    context = {}
+    return context
+
+def login(request):
+    context = {}
+    return context
 
 # Setup and start app
 if __name__ == '__main__':
     with Configurator() as config:
         
-        # app includes
         config.include("pyramid_jinja2")
+        config.add_jinja2_renderer(".html")
+        config.add_jinja2_search_path("./templates", name=".html")
         
-        # add rtoutes
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
+        # routes
+        config.add_route('home', '/')
+        config.add_route('signup', '/signup')
+        config.add_route('login', '/login')
+        
+        # views
+        config.add_view(home, route_name='home', renderer="home.html")
+        config.add_view(signup, route_name='signup', renderer="signup.html")
+        config.add_view(login, route_name='login', renderer="login.html")
         
         # Setup wsgi
         app = config.make_wsgi_app()
