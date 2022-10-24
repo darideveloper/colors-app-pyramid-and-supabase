9918ee3ce1c5c618e35bfca9d8262045781b1a97
daridev <hernandezdarifrancisco@gmail.com>
2022-10-24 15:15:52 -0500

    sign up

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 import os
 import mimetypes
+from unicodedata import name
 from wsgiref.simple_server import make_server
+from psutil import users
 from pyramid.config import Configurator
 from pyramid.response import Response
 from supabase import create_client as sb_create_client, Client as sb_client
@@ -16,23 +18,51 @@ project_url = credentials.get ("supabase_project_url")
 supabase: sb_client = sb_create_client(project_url, secret)
 
 def home(request):
-    context = {"current_page": "Home"}
+    context = {
+        "current_page": "Home",
+        "error": "",
+        "message": "",
+    }
     return context
 
 def signup(request):
     # Get ikmages path
-    context = {"current_page": "Signup"}
+    context = {
+        "current_page": "Signup",
+        "error": "",
+        "message": "",
+    }
     
     # Get form data in post
     if request.method == 'POST':
-        user = request.params["user"]
         email = request.params["email"]
         password = request.params["password"]
+        
+        # Validateif the user email already exist
+        users = supabase.auth.api.list_users ()
+        users_found = list(filter (lambda user: user.email == email, list(users)))
+        users_found_num = len(users_found)
+        
+        # Show error if user exist
+        if users_found_num == 1:
+            context["error"] = f"The user '{email}' already exist"
+        
+        # Register new user with email
+        elif users_found_num == 0:
+            user = supabase.auth.sign_up (email=email, password=password)
+            context["message"] = f"Done. Check your inbox to verity your email"
+            
+            
+        
     
     return context
 
 def login(request):
-    context = {"current_page": "Login"}
+    context = {
+        "current_page": "Login",
+        "error": "",
+        "message": "",
+    }
     return context
 
 # Setup and start app
@@ -59,6 +89,6 @@ if __name__ == '__main__':
         
     # Run
     host = "0.0.0.0"
-    port = 6543
+    port = 3000
     print (f"Running app, open at: http://localhost:{port}/")
     make_server(host=host, port=port, app=app).serve_forever()
